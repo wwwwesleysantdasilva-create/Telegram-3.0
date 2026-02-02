a0cc885126084fe3f7353576055692b3d2ace86d
wwwwesleysantdasilva-create <wwwwesleysantdasilva@gmail.com>
2026-02-02 10:53:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fs from "fs";
 
 /* ================= CONFIG FIXA ================= */
 
-const BOT_TOKEN = 8579716346:AAEDahqK3E9Yub74sUr9Col6aBIyjd0BQSQ;
+const BOT_TOKEN = "8579716346:AAEDahqK3E9Yub74sUr9Col6aBIyjd0BQSQ";
 const MASTER_ADMIN = 8235876348;
 const LOG_GROUP_ID = -5164103528;
 
